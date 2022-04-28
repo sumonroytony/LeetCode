b03f87944a8c20c38fee472214131e0874500bd0
tony <sumonroytony@gmail.com>
2022-04-28 22:41:53 +0600

    tried again palindrome number

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -1,23 +1,15 @@
-//solution 1
-
-// var isPalindrome = function (x) {
-//   let palindrome = '';
-//   x.toString()
-//     .split("")
-//     .forEach((el) => {
-//         palindrome = el + palindrome;
-//     });
-//   if (x === Number(palindrome)) {
-//     return true;
-//   } else return false;
-// };
-
-//solution 2
-var isPalindrome = function (x) {
-  function palindrome() {
-    return x.toString().split("").reverse().join("");
+const returnModules = (divider, remainder) => {
+  if (divider < 1) {
+    return remainder;
+  } else {
+    remainder = remainder * 10 + (divider % 10);
+    return returnModules(Math.trunc(divider / 10), remainder);
   }
-  if (x === Number(palindrome())) {
-    return true;
-  } else return false;
 };
+
+var isPalindrome = function (x) {
+  if (returnModules(x, 0) === x) return true;
+  else return false;
+};
+
+console.log(isPalindrome(121));
