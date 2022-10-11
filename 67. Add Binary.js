5f55ebf65edf224323bf8ef854d0de379159a254
ton <tony@workspaceit.com>
2022-10-11 17:52:34 +0600

    added 67

---
+++ 67. Add Binary.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+ var addBinary = function(a, b) {
+    let bigString;
+    let smallString;
+    let carry= 0;
+    let result='';
+    if(a.length > b.length) {
+        bigString = a;
+        smallString = b
+    } else {
+        bigString = b;
+        smallString = a;
+    }
+
+    for(let i = 1; i<= bigString.length || carry > 0; i++){
+        
+        let small = smallString[smallString.length - i] || 0
+        let big = bigString[bigString.length -i] || 0
+        const tempResult= Number(big) + Number(small) + carry;
+        if(carry > 0) carry--;
+        if(tempResult >=2) {
+            result = '1' + result;
+            carry = carry + tempResult;
+        } else result = `${tempResult}${result}`
+       
+        console.log({ carry})
+    }
+    return result;
+};
+
+
+console.log(addBinary('11', '10'))
