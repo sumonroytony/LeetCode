8204c6ebd74af29cb0727ad6421f4b35cae2496d
tony <sumonroytony@gmail.com>
2022-11-30 00:17:11 +0600

    581 not completed

---
+++ 581. Shortest Unsorted Continuous Subarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function (nums) {
+  let startProblem = 0;
+  let endProblem = nums[nums.length - 1];
+
+  for (let i = 0; i < nums.length; i++) {
+    i;
+  }
+};
+
+console.log(findUnsortedSubarray([1, 3, 2, 2, 2]));
